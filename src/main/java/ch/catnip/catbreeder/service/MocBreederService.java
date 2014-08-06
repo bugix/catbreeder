d49efcaa746786356361792c1f1efd14b5668c25
Martin Imobersteg <martin.imobersteg@gmail.com>
2014-08-06 17:45:07 +0200

    Add Grumpy as moc picture

--- src/main/java/ch/catnip/catbreeder/service/MocBreederService.java
+++ src/main/java/ch/catnip/catbreeder/service/MocBreederService.java
@@ -1,13 +1,20 @@
 package ch.catnip.catbreeder.service;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDate;
 import java.util.LinkedList;
 import java.util.List;
 
-import javax.servlet.ServletContext;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+
+import com.google.common.io.ByteStreams;
 
 import ch.catnip.catbreeder.model.Breed;
 import ch.catnip.catbreeder.model.Breeder;
@@ -15,29 +22,35 @@ import ch.catnip.catbreeder.model.Cat;
 
 @Service
 public class MocBreederService implements BreederService {
-	
+
+	private static final transient Logger logger = LoggerFactory.getLogger(MocBreederService.class);
+
+	private final Breeder currentBreeder;
+
+	private final List<Cat> catList;
+
 	@Autowired
-	ServletContext context;
-	
-	private static Breeder currentBreeder;
-	
-	private static List<Cat> catList;
-	
-	public MocBreederService() {
+	public MocBreederService(ApplicationContext applicationContext) {
 		currentBreeder = new Breeder();
 		currentBreeder.setFirstName("John");
 		currentBreeder.setLastName("Doe");
 		currentBreeder.setLogin("john");
 		currentBreeder.setPassword("123456");
-		
-		//context.getResourceAsStream("grumpy.jpg")
+
+		byte[] grumpyBytes = null;
+
+		try {
+			grumpyBytes = ByteStreams.toByteArray(applicationContext.getResource("classpath:grumpy.jpg").getInputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		catList = new LinkedList<Cat>();
-		catList.add(new Cat("Anonymous", currentBreeder, Breed.LONGHAIR, LocalDate.now(), null));
-		catList.add(new Cat("Brutus", currentBreeder, Breed.SHORTHAIR, LocalDate.now(), null));
-		catList.add(new Cat("PussyCat", currentBreeder, Breed.SHORTHAIR, LocalDate.now(), null));
-		catList.add(new Cat("Wotan", currentBreeder, Breed.LONGHAIR, LocalDate.now(), null));
-		catList.add(new Cat("X-Cat", currentBreeder, Breed.NAKED, LocalDate.now(), null));
+		catList.add(new Cat("Anonymous", currentBreeder, Breed.LONGHAIR, LocalDate.now(), grumpyBytes));
+		catList.add(new Cat("Brutus", currentBreeder, Breed.SHORTHAIR, LocalDate.now(), grumpyBytes));
+		catList.add(new Cat("PussyCat", currentBreeder, Breed.SHORTHAIR, LocalDate.now(), grumpyBytes));
+		catList.add(new Cat("Wotan", currentBreeder, Breed.LONGHAIR, LocalDate.now(), grumpyBytes));
+		catList.add(new Cat("X-Cat", currentBreeder, Breed.NAKED, LocalDate.now(), grumpyBytes));
 	}
 
 	@Override
@@ -53,9 +66,9 @@ public class MocBreederService implements BreederService {
 
 	@Override
 	public boolean loginBreeder(String login, String password) {
-		
-		if (!"john".equals(currentBreeder.getLogin()) || !"123456".equals(currentBreeder.getPassword()))
-		{
+
+		if (!"john".equals(currentBreeder.getLogin())
+				|| !"123456".equals(currentBreeder.getPassword())) {
 			return false;
 		}
 
